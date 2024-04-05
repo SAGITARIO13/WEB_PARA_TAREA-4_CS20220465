6cf4191620305b3dc4651a7ac1c941a19a2247bb
CarlosSanchezQATester <csanchez@cloudservices.com.do>
2024-04-05 00:57:37 -0400

    procesos php para manejos de peticiones al servidor

---
+++ home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <a href="crear_publicacion.html">Crear Publicación</a>
+        <a href="editar_perfil.html">Editar Perfil</a>
+        <a href="home.php">Buscar Publicaciones</a>
+    </nav>
+    <h1>Publicaciones</h1>
+    <form action="" method="get">
+        <label for="search">Buscar:</label>
+        <input type="text" id="search" name="search">
+        <input type="submit" value="Buscar">
+    </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Título</th>
+                <th>Contenido</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            
+            $conexion = mysqli_connect("localhost", "root", "", "myweb");
+
+        
+            $search_term = isset($_GET["search"]) ? $_GET["search"] : '';
+
+            $query = "SELECT * FROM publicaciones";
+            if (!empty($search_term)) {
+                $query .= " WHERE title LIKE '%$search_term%' OR content LIKE '%$search_term%'";
+            }
+
+            
+            $result = mysqli_query($conexion, $query);
+
+            
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['title'] . "</td>";
+                echo "<td>" . $row['content'] . "</td>";
+                echo "</tr>";
+            }
+          
+            mysqli_close($conexion);
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ procesar_inicio_sesion.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login_email"]) && isset($_POST["login_password"])) {
+    $email = $_POST["login_email"];
+    $password = $_POST["login_password"];
+
+    $conexion = mysqli_connect("localhost", "root", "", "myweb");
+    $query = "SELECT * FROM usuarios WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($conexion, $query);
+
+ if ($result && mysqli_num_rows($result) > 0) {
+ 
+    $_SESSION["loggedin"] = true;
+    $_SESSION["username"] = $username;
+    header("Location: home.php");
+    exit;
+    } 
+    else {
+    $error_message = "Credenciales incorrectas. Por favor, inténtalo de nuevo.";
+    }
+ 
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procesar Inicio de Sesión</title>
+</head>
+<body>
+    <?php if (isset($error_message)) : ?>
+        <div>
+            <p><?php echo $error_message; ?></p>
+        </div>
+    <?php endif; ?>
+</body>
+</html>
